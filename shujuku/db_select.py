ea2ac632c90841734ac6dbbedd24ec1adc866ef0
hhh <411543589@qq.com>
2018-12-18 16:23:12 +0800

    mySQL
    数据库数据处理 网址 名称处理

--- shujuku/db_select.py
+++ shujuku/db_select.py
@@ -4,29 +4,47 @@
 import pymysql
 
 # 打开数据库连接
-db = pymysql.connect("localhost", "tracert1", "tracert1", "tracert", charset='utf8' )
+db = pymysql.connect("localhost", "root", "", "tracert", charset='utf8' )
+
+### select `处理网址`,处理名称 from db_address_wai GROUP BY `处理网址`
 
 # 使用cursor()方法获取操作游标 
 cursor = db.cursor()
 
 # SQL 查询语句
-sql = "SELECT * FROM LOGS_TRACERT \
-       WHERE DB_NAME = '%s'" % ('ASME')
+### sql = "SELECT * FROM LOGS_TRACERT WHERE DB_NAME = '%s'" % ('ASME')
+
+sql = "select `处理网址`,处理名称 from db_address_wai GROUP BY `处理网址`"
+### f = open("file1", "r")  # 打开文件
+fnew = open("c:\\tracert\\Tracert_All_GBK.bat", "w", encoding="GBK")
+
+
 try:
    # 执行SQL语句
    cursor.execute(sql)
    # 获取所有记录列表
    results = cursor.fetchall()
    for row in results:
-      IP_ADRESS = row[0]
-      STATUS = row[1]
-      CONTENT = row[2]
-      DATE_TIME = row[3]
-      DB_NAME = row[4]
+#      IP_ADRESS = row[0]
+#      STATUS = row[1]
+#      CONTENT = row[2]
+#      DATE_TIME = row[3]
+#      DB_NAME = row[4]
+      IP_ADRESS =row[0].strip()
+      DB_NAME = row[1].replace(' ','')
       # 打印结果
-      print ("IP_ADRESS=%s,STATUS=%d,CONTENT=%s,DATE_TIME=%s,DB_NAME=%s"%(IP_ADRESS, STATUS, CONTENT, DATE_TIME, DB_NAME ))
+#      print ("IP_ADRESS=%s,STATUS=%d,CONTENT=%s,DATE_TIME=%s,DB_NAME=%s"%(IP_ADRESS, STATUS, CONTENT, DATE_TIME, DB_NAME ))
+      print ("IP_ADRESS=%s,DB_NAME=%s"%(IP_ADRESS, DB_NAME))
+      fnew.write('tracert '+IP_ADRESS+' >c:\\tracert\\'+ DB_NAME +'.txt')
+      fnew.write('\n')
 except:
    print ("Error: unable to fecth data")
 
 # 关闭数据库连接
 db.close()
+fnew.close()  #关闭文件
+
+
+### Version 2018-12-18 16:15
+
+
--- shujuku/db_update.py
+++ shujuku/db_update.py
@@ -4,13 +4,21 @@
 import pymysql
 
 # 打开数据库连接
-db = pymysql.connect("localhost", "tracert1", "tracert1", "tracert", charset='utf8' )
+db = pymysql.connect("localhost", "root", "", "tracert", charset='utf8' )
 
 # 使用cursor()方法获取操作游标 
 cursor = db.cursor()
 
 # SQL 更新语句
-sql = "UPDATE EMPLOYEE1 SET INCOME = INCOME - 100 WHERE SEX = '%c'" % ('M')
+sql = "UPDATE db_address_wai SET 处理网址 = trim(trailing '/' from  (trim(leading 'http://' from 网址))) WHERE 网址 like " +'"' + 'http://%'+'"'
+
+### '''sql = "UPDATE db_address_wai SET 处理网址 = trim(trailing '/' from  (trim(leading 'http://' from 网址))) WHERE 数据库名称 = 'SCI / CPCI-S'"'''
+
+### trim(leading 'http://' from
+### trim(leading 'https://' from
+### trim(trailing '/' from
+### WHERE 数据库名称 = 'SCI / CPCI-S'
+
 try:
    # 执行SQL语句
    cursor.execute(sql)
@@ -24,3 +32,5 @@ except:
 
 # 关闭数据库连接
 db.close()
+
+### Version Time 2018-12-17 17:06
